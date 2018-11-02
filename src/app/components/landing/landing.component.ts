cd4a498aa634958254ff2141557e55553caec951
Arif <arifnadeem7@gmail.com>
2018-11-03 04:31:01 +0530

    Changed landing page design, added form validation to business leads and login email address field, performance improvements and other fixes

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,25 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, ElementRef, AfterViewInit } from '@angular/core';
 import { BusinessleadsService } from 'src/app/services/businessleads.service';
+import { headersToString } from 'selenium-webdriver/http';
+
+declare const window: any;
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('f') form;
+  @ViewChild('header') header: ElementRef;
   model: any = {};
   loading = false;
   public show: boolean = false;
+  public contentScrolled: boolean = false;
+  private scrollHeight: number = 0;
 
   constructor(private businessLeadsService: BusinessleadsService) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.scrollHeight = this.header.nativeElement.offsetHeight;
+  }
+
+  scroll(el) {
+    el.scrollIntoView({ behavior: "smooth" });
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > (this.scrollHeight - (this.scrollHeight / 2))) {
+      this.contentScrolled = true;
+    } else {
+      this.contentScrolled = false;
+    }
+  }
+
   businessLeads() {
     this.loading = true;
     this.businessLeadsService.businessLead(this.model.email, this.model.phone).subscribe(success => {
+      this.form.resetForm();
       this.loading = false;
       this.show = true;
       setTimeout(function () {
