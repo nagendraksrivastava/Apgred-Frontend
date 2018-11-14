2678c5f7e18a536f5ab00fccd25c5769b3ef29b3
Arif Nadeem <arifnadeem7@gmail.com>
2018-11-14 15:21:00 +0530

    send hash with businessleads api

--- src/app/services/businessleads.service.ts
+++ src/app/services/businessleads.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ApiEndPoints } from '../constants/api.endpoints';
+import CryptoJS from 'crypto-js';
+import { LocalStorageService } from './local-storage.service';
 
 let business_leads_endpoint = "/businesslead/"
 
@@ -13,13 +15,28 @@ export interface BusinessLeadResponse {
 })
 export class BusinessleadsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) { }
 
-  public businessLead(email_address: string, phone: string){
-     return this.http.post<BusinessLeadResponse>(ApiEndPoints.BASE_URL + business_leads_endpoint,
+  public businessLead(email_address: string, phone: string) {
+    let loggedInUser = this.localStorageService.get('loggedInUser');
+    if (loggedInUser && loggedInUser.token) {
+      this.localStorageService.remove(loggedInUser)
+    }
+    let items: Array<string> = [];
+    items.push(email_address)
+    items.push(phone)
+    items.reverse()
+    var crypt: string = items.join(' ')
+    var hash = CryptoJS.SHA1(crypt);
+    let digest = CryptoJS.enc.Hex.stringify(hash);
+    let httpHeaders = new HttpHeaders().set('HASH', digest);
+    return this.http.post<BusinessLeadResponse>(ApiEndPoints.BASE_URL + business_leads_endpoint,
       {
         email: email_address,
         phone_number: phone
+      },
+      {
+        headers: httpHeaders
       })
   }
 }
